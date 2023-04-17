fc67b48a1d33d3b66449f85fb7bac78027f59375
Anton3421 <bgx169@myy.haaga-helia.fi>
2023-04-17 23:44:20 +0300

    Adding new method

--- Bookstore/src/main/java/com/project/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/project/Bookstore/web/BookController.java
@@ -1,20 +1,11 @@
 package com.project.Bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.viikko2.exercise2.domain.Friend;
 
 
-@Controller
+
 public class BookController {
 
 	@RequestMapping(value="/index", method=RequestMethod.GET)
@@ -22,15 +13,5 @@ public class BookController {
 		return null;
 		
 	}	
-	@RequestMapping(value = "/booklist", method = RequestMethod.POST)
-	public String friendSubmit(@ModelAttribute Friend name, Model model) {
-		
-		
-		List<Friend> list = new ArrayList<Friend>();		
-		list.add(name);
-		
-		model.addAttribute("friends", list);
-		
-		return "booklist";
-	}
 }
+
