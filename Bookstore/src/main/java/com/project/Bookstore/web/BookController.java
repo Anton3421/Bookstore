0cc54086fbac09d576f7005ba61f524260c5ec58
Anton3421 <bgx169@myy.haaga-helia.fi>
2023-04-19 14:12:05 +0300

    Adding new method

--- Bookstore/src/main/java/com/project/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/project/Bookstore/web/BookController.java
@@ -2,10 +2,12 @@ package com.project.Bookstore.web;
 
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import com.project.Bookstore.domain.Book;
+import com.project.Bookstore.domain.BookRepository;
 
 public class BookController {
 
@@ -14,6 +16,11 @@ public class BookController {
 		return null;
 		
 	}	
-	
+	@GetMapping("/booklist")
+	public String getBookList(Model model) {
+		model.addAttribute("bookList", repository.findAll());
+		System.out.println("PROGRAM READS: " + repository.findAll());
+		return "bookList";
+}
 }
 
