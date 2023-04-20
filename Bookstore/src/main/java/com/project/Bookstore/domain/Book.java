7bf1f318f0c36a31e2ca8a777c391f7555628b64
Anton3421 <bgx169@myy.haaga-helia.fi>
2023-04-20 19:05:36 +0300

    Adding new method

--- Bookstore/src/main/java/com/project/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/com/project/Bookstore/domain/Book.java
@@ -1,4 +1,5 @@
 package com.project.Bookstore.domain;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +9,7 @@ import jakarta.persistence.Id;
 public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	private Long id;
 	private String title;
 	private String author;
 	private String publicationYear;
@@ -25,6 +26,15 @@ public class Book {
 		this.isbn = isbn;
 		this.price = price;
 	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getTitle() {
 		return title;
 	}
--- Bookstore/src/main/java/com/project/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/project/Bookstore/web/BookController.java
@@ -1,12 +1,17 @@
 package com.project.Bookstore.web;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.Bookstore.domain.Book;
 import com.project.Bookstore.domain.BookRepository;
@@ -17,15 +22,32 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String Book(Model model) {
-		return null;
-		
-	}	
 	@RequestMapping(value="/booklist", method=RequestMethod.GET)
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "bookList";
-}
+	}
+	@GetMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addBook";
+	}
+	@PostMapping(value = "/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:/booklist";
+	}
+	
+	@GetMapping(value = "/delete/{id}")
+	
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	@GetMapping(value = "/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editBook";
+	}
 }
 
