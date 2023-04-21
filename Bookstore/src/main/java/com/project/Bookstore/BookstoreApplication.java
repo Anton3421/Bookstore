46904941331150c4b4a8e0ddd1df21320edf3538
Anton3421 <bgx169@myy.haaga-helia.fi>
2023-04-21 21:59:10 +0300

    Adding new method

--- Bookstore/src/main/java/com/project/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/project/Bookstore/BookstoreApplication.java
@@ -2,6 +2,8 @@ package com.project.Bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +13,12 @@ import com.project.Bookstore.domain.BookRepository;
 import com.project.Bookstore.domain.Category;
 import com.project.Bookstore.domain.CategoryRepository;
 
+
+
 @SpringBootApplication
 public class BookstoreApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
